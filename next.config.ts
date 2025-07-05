21847aaf952ae712c234bd6f5c882d327bca7f39
MozartOnWeb <mozartontheweb@gmail.com>
2025-07-05 15:12:54 +0000

    chore: update dependencies and add new styles
    - Updated Next.js to version 15.3.0.
    - Added Sass as a dependency.
    - Reorganized devDependencies to include TypeScript after other type definitions.
    - Added multiple Parcel watcher packages for improved file watching across platforms.
    - Added new dependencies for improved functionality (e.g., chokidar, micromatch).
    - Created a new SCSS module file for styling.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  sassOptions: {
+    // additionalData: `$var: red;`,
+    // implementation: "scss",
+  },
 };
 
 export default nextConfig;
